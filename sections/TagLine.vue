90aba3a5299e2c6a5d88da67f45bef4abf5fc094
paris <silvester.pari@eox.at>
2019-04-07 21:38:30 +0200

    added sections

---
+++ sections/TagLine.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="tagline">
+    <h1 id="pu-slogan">
+      Klassisch, aber Leiwand<span class="highlight">*</span>
+    </h1>
+    <h2 id="pu-explanation">
+      <span class="highlight">*</span>Klassische Musik trifft gemütliche locations. Nimm dir ein Getränk, lehn dich zurück, lass deine Gedanken schweifen. Erlebe gute Musik Live, hautnah und zum anfassen.
+    </h2>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
