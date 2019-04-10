bbc2f46c52187e7ea947196c95cfe1ab163628bd
paris <silvester.pari@eox.at>
2019-04-10 17:46:15 +0200

    added event store

---
+++ store/events.js
@@ -0,0 +1,116 @@
+export const state = () => ({
+  events: [
+    {
+      id: 7,
+      title: 'PopUp Concert #07',
+      body: `KLASSISCH &amp; ANDERS<br><br>Es geht weiter! Diesmal mit einer exklusiven und ungewöhnlichen Mischung: Posaune &amp; Schlagwerk.<br>Schau im Ludwig &amp; Adele vorbei &amp; schau's dir an - ein wenig klassisch, ein wenig anders!<br><br>WO<br>Kollektiv Ludwig &amp; Adele<br>Im Künstlerhaus<br>Akademiestraße 13<br>1010 Wien<br><br>WANN<br>Freitag, 12. April 2019 <br>Einlass 21:30 | Beginn 22:00<br><br>WER<br>Gabriel Antão | Posaune<br>Joachim Murnig | Schlagwerk<br><br>WAS<br>Bernstein, Jobim, Zambito, Calissi, Wright  +<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org`,
+      fbLink: 'https://www.facebook.com/events/579677072529255/',
+      cover: '/images/puc07.jpg',
+      startDate: '12 Apr 2019 22:00:00 GMT+0200',
+      endDate: '12 Apr 2019 23:30:00 GMT+0200',
+      slug: 'popup-concert-07',
+      location: {
+        locationName: 'Kollektiv Ludwig & Adele',
+        street: 'Akademiestraße 13',
+        postCode: '1010',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 6,
+      title: 'PopUp Concert #06',
+      body: `IN VINO CLASSICAL<br><br>Wir freuen uns mit euch auf das nächste feine Konzert mit dem spannenden Duo Dalia Dedinskaite &amp; Gleb Pysniak! Ein berauschendes Klassikerlebnis, unterstützt durch ein paar Glaserl Wein (rechtzeitig zur Verkostung kommen!).<br><br>WO<br>Vinoto<br>Schönbrunner Straße 55<br>1050 Wien<br><br>WANN<br>Freitag, 16. März 2018 <br>Einlass &amp; Weinverkostung 19:00 | Beginn 19:30<br><br>WER<br>Dalia Dedinskaite | Violine<br>Gleb Pysniak | Violoncello<br><br>WAS<br>Kodály, Ravel, Händel, Igudesman, Barkauskas +<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org`,
+      fbLink: 'https://www.facebook.com/events/1796134727356635/',
+      cover: '/images/puc06.jpg',
+      startDate: '16 Mar 2018 19:00:00 GMT+0200',
+      endDate: '16 Mar 2018 20:30:00 GMT+0200',
+      slug: 'popup-concert-06',
+      location: {
+        locationName: 'Vinoto',
+        street: 'Schönbrunner Straße 55',
+        postCode: '1050',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 5,
+      title: 'PopUp Concert #05',
+      body: `IT'S CLASSICAL, BABY<br><br>Wir freuen uns mit euch auf ein feines Konzert mit Klaviertrio! Geige, Cello, Klavier: perfekte Kombination, was will man mehr. Spannende junge Musiker und gemütliche Atmosphäre - klassisch, aber leiwand!<br><br>WO<br>Cafe Korb | Art Lounge<br>Brandstätte 9<br>1010 Wien<br><br>WANN<br>Mittwoch, 17. Jänner 2018 <br>Einlass 19:30 | Beginn 20:00<br><br>WER<br>Wilfried Kazuki Hedenborg | Violine<br>Bernhard Naoki Hedenborg | Violoncello<br>Julian Yo Hedenborg | Klavier<br><br>WAS<br>Beethoven | Kreisler | Rachmaninoff u.a.<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org`,
+      fbLink: 'https://www.facebook.com/events/176436982910708/',
+      cover: '/images/puc05.jpg',
+      startDate: '17 Jan 2018 20:00:00 GMT+0200',
+      endDate: '17 Jan 2018 22:00:00 GMT+0200',
+      slug: 'popup-concert-05',
+      location: {
+        locationName: 'Cafe Korb | Art Lounge',
+        street: 'Brandstätte 9',
+        postCode: '1010',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 4,
+      title: 'PopUp Concert #04',
+      body: `DAS IST MUSIK, MEIN FREUND<br><br>PopUp Concert #04 bietet euch eine ungewöhnliche Kombination: Klarinette und Kontrabass hört man nicht alle Tage im Duo!<br>Vera Karner und Dominik Wagner, beide Rising Stars und normalerweise auf den großen Bühnen dieser Welt zu hören, spielen für euch im Mon Ami - hautnah und zum Anfassen, sozusagen.<br>Gemütlich etwas trinken und auf Loungesesseln chillen - so macht Klassik Spaß!<br><br>WO<br>Mon Ami<br>Theobaldgasse  9/1a<br>1060 Wien<br><br>WANN<br>Mittwoch, 15. November 2017 <br>Einlass 19:00 | Beginn 19:30 | DJ-Line ab 22:00<br><br>WER<br>Vera Karner, Klarinette<br>Dominik Wagner, Kontrabass <br><br>WAS<br>Akos Banlaky: Suite für Kontrabass und Klarinette<br>Beethoven: Duo für Fagott und Klarinette (bearb.)<br>Breinschmid: miniatures<br><br>Wolfram Wagner: Solo Sonate 4. und 5. Satz, Vivace für Solo Kontrabass<br>Teppo Hauta-aho: Kadenza<br>Bach: Sarabande aus der 3. Suite<br><br>Kovacs: Hommage an Richard Strauss<br>Kovacs: Hommage an M. De Falla<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org<br>#popupconcert`,
+      fbLink: 'https://www.facebook.com/events/209890312882854/',
+      cover: '/images/puc04.jpg',
+      startDate: '15 Nov 2017 19:30:00 GMT+0200',
+      endDate: '15 Nov 2017 21:30:00 GMT+0200',
+      slug: 'popup-concert-04',
+      location: {
+        locationName: 'Mon Ami',
+        street: 'Theobaldgasse 9/1a',
+        postCode: '1060',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 3,
+      title: 'PopUp Concert #03',
+      body: `KLASSISCH, ABER LEIWAND<br><br>Ein klassisches Streichquartett im phil! Bewegende Musik zum Sinnieren, Genießen und Träumen, umgeben von phielen Büchern und leiwander Stimmung. Letztes PopUp Concert vor dem Sommer!<br><br>WO<br>phil<br>Gumpendorferstraße  10-12<br>1060 Wien<br><br>WANN<br>Dienstag, 20. Juni 2017 <br>Einlass 19:00 | Beginn 19:30 | DJ-Line ab 22:00<br><br>WER<br>Alliance Quartett Wien | http://www.alliancequartett.at/<br>Robert Olisa Nzekwu, Violine<br>Katharina Henriquez, Violine<br>Joachim Kelber, Viola<br>Johanna Kotschy-Appel, Violoncello<br><br>WAS<br>Ludwig van Beethoven | Robert Schumann | Leoš Janáček <br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org<br>#popupconcert`,
+      fbLink: 'https://www.facebook.com/events/258907394584421/',
+      cover: '/images/puc03.jpg',
+      startDate: '20 Jun 2017 19:30:00 GMT+0200',
+      endDate: '20 Jun 2017 21:30:00 GMT+0200',
+      slug: 'popup-concert-03',
+      location: {
+        locationName: 'phil',
+        street: 'Gumpendorferstraße 10-12',
+        postCode: '1060',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 2,
+      title: 'PopUp Concert #02',
+      body: `CLASSICAL IS THE NEW HIPSTER<br><br>PopUp Concerts bringt pures Musikerlebnis in das SUPERSENSE VIENNA! Im venezianischen Dogenpalast - inmitten von Letterpress-Drucken, Schallplatten, Polaroidfotografien und einem Smell Lab - erklingen Werke von Bach, Beethoven, Chopin, Ysaÿe, Enescu, Prokofiev, Ernst, Salgado.<br><br>WO<br>SUPERSENSE<br>Praterstraße 70<br>1020 Wien<br><br>WANN<br>Freitag, 19. Mai 2017 <br>Einlass 19:00 | Beginn 19:30<br><br>WER<br>Emmanuel Tjeknavorian | http://emmanueltjeknavorian.com<br>Andrés Añazco | http://andresanazco.com<br><br>WAS<br>Bach | Beethoven | Chopin | Ysaÿe | Enescu | Prokofiev | Ernst | Salgado<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org<br>#popupconcert`,
+      fbLink: 'https://www.facebook.com/events/1878004695801411/',
+      cover: '/images/puc02.jpg',
+      startDate: '19 May 2017 19:30:00 GMT+0200',
+      endDate: '19 May 2017 21:30:00 GMT+0200',
+      slug: 'popup-concert-02',
+      location: {
+        locationName: 'SUPERSENSE',
+        street: 'Praterstraße 70',
+        postCode: '1020',
+        city: 'Vienna'
+      }
+    },
+    {
+      id: 1,
+      title: 'PopUp Concert #01',
+      body: `EVERYBODY LOVES CLASSICAL MUSIC -<br>nur manche wissen's noch nicht.<br><br>PopUp Concerts bringt Musik zum Hören, zum Sehen, zum Spüren, zum Genießen, zum Angreifen, zum Trinken, zum Erleben in das wunderbare Cafe Korb. Erlebt mit uns klassische Musik hautnah!<br><br>WO<br>Cafe Korb | Art Lounge<br>Brandstätte 9<br>1010 Wien<br><br>WANN<br>Samstag, 22. April 2017 <br>Einlass 19:00 | Beginn 19:30<br><br>WER<br>Piotr Koscik, Klavier | piotrkoscik.com<br>Alejandro Loguercio, Violine | alejandrologuercio.com<br>Una Smrekar, Klavier | unasmrekar.com<br><br>WAS<br>Mozart | Brahms | Chopin | Piazzolla u.a.<br><br>WIEVIEL<br>Pay as you wish!<br>(Freie Spende)<br><br>facebook.com/popupconcerts<br>popupconcerts.org`,
+      fbLink: 'https://www.facebook.com/events/503445953376959/',
+      cover: '/images/puc01.jpg',
+      startDate: '22 Apr 2017 19:30:00 GMT+0200',
+      endDate: '22 Apr 2017 21:30:00 GMT+0200',
+      slug: 'popup-concert-01',
+      location: {
+        locationName: 'Cafe Korb | Art Lounge',
+        street: 'Brandstätte 9',
+        postCode: '1010',
+        city: 'Vienna'
+      }
+    }
+  ]
+})
