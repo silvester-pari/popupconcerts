76e2732f67f83691db7f7adb32837583828e81d8
paris <silvester.pari@eox.at>
2019-04-11 20:46:10 +0200

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>:(</h1>
+    <h2>404: Page not found</h2>
+    <p>Sorry, das hat nicht geklappt! Versuch's mal mit einer anderen Seite...</p>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
